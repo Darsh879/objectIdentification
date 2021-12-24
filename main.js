9fa73440e00c4fe413cb93b751857f453dadd1a7
Darsh879 <79584169+Darsh879@users.noreply.github.com>
2021-12-24 20:36:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+img = "";
+object = [];
+status = "";
+function setup()
+{
+    canvas = createCanvas(640, 420);
+    canvas.center();
+
+    obejectDetector = ml5.objectDetector("cocossd", modelLoaded);
+}
+
+function modelLoaded()
+{
+console.log("modelLoaded")
+status = true;
+objectDetector.detect(img, gotResult);
+}
+
+function preload()
+{
+    img = loadImage("dog_cat.jpg")
+}
+
+function draw()
+{
+    image(img, 0, 0, 640, 420);
+    if(status != "")
+{
+    for(i=0; i<objects.length; i++)
+    {
+        document.getElementById("status").innerHTML = "status : object Detected";
+fill("red");
+percent = floor(objects[i].confidence * 100);
+text(objects[i].label + " " + percent + "%", objects[i].x + 15 , objects[i].y + 15);
+noFill();
+stroke("red");
+rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+    }
+}
+}
+
+function gotResult(error, results)
+{
+if(error)
+{
+    console.error(error);
+}
+console.log(results);
+objects = results;
+}
+
